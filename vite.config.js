d05db85390d912014ea13ae7c4da84c7768abe42
manubg <bgmanu2426@gmail.com>
2025-02-05 18:19:52 +0530

    set base path for deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Reactive-React-Forms/', // Add this line
   plugins: [react()],
 })
